3c01e3c2f9f1651ab294287f2cb69e0e31c840f6
luongdthong2889 <luong.dthong@gmail.com>
2021-05-15 03:41:26 +0700

    commit 15/5/2021
    commit 15/5/2021

---
+++ Admin/DemoDB2/DemoDB2/Controllers/RaoVatController.cs
@@ -0,0 +1,102 @@
+﻿using DemoDB2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoDB2.Controllers
+{
+    public class RaoVatController : Controller
+    {
+        DBRaoVatEntities db = new DBRaoVatEntities();
+        // GET: RaoVat
+        //public ActionResult Index()
+        //{
+        //    return View(db.RAOVATs.ToList());
+        //}
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(RAOVAT x)
+        {
+            try
+            {
+                db.RAOVATs.Add(x);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return Content("Error Create New");
+            }
+        }
+        public ActionResult Details(int id)
+        {
+            return View(db.RAOVATs.Where(s => s.MATIN == id).FirstOrDefault());
+        }
+        public ActionResult Edit(int id)
+        {
+            return View(db.RAOVATs.Where(s => s.MATIN == id).FirstOrDefault());
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, RAOVAT x)
+        {
+            db.Entry(x).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            return View(db.RAOVATs.Where(s => s.MATIN == id).FirstOrDefault());
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, RAOVAT x)
+        {
+            try
+            {
+                x = db.RAOVATs.Where(s => s.MATIN == id).FirstOrDefault();
+                db.RAOVATs.Remove(x);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return Content("This data is using in other table, Error Delete!");
+            }
+        }
+        public ActionResult Index(string _name)
+        {
+            if (_name == null)
+                return View(db.RAOVATs.ToList());
+            else
+                return View(db.RAOVATs.Where(s => s.TIEUDE.Contains(_name)).ToList());
+        }
+        public ActionResult SelectCate()
+        {
+            CATEGORY se_cate = new CATEGORY();
+            se_cate.ListCate = db.CATEGORies.ToList<CATEGORY>();
+            return PartialView(se_cate);
+        }
+        public ActionResult SelectTinhTrang()
+        {
+            TINHTRANG se_tinhtrang = new TINHTRANG();
+            se_tinhtrang.ListTinhTrang = db.TINHTRANGs.ToList<TINHTRANG>();
+            return PartialView(se_tinhtrang);
+        }
+        public ActionResult SelectLoaiTin()
+        {
+            LOAITIN se_loaitin = new LOAITIN();
+            se_loaitin.ListLoaiTin = db.LOAITINs.ToList<LOAITIN>();
+            return PartialView(se_loaitin);
+        }
+        public ActionResult SelectHinhThuc()
+        {
+            HINHTHUC se_hinhthuc = new HINHTHUC();
+            se_hinhthuc.ListHinhThuc = db.HINHTHUCs.ToList<HINHTHUC>();
+            return PartialView(se_hinhthuc);
+        }
+    }
+}
---
+++ User/RaoVat/RaoVat/Controllers/CategoriesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RaoVat.Models;
+using PagedList;
+
+namespace RaoVat.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        DBRaoVatEntities db = new DBRaoVatEntities();
+        public ActionResult Detail(int? id)
+        {
+           if(id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            return View(db.RAOVATs.Where(x => x.MATIN == id).FirstOrDefault());
+        }
+        public ActionResult Index(string category)
+        {
+            if (category == null)
+            {
+                var list = db.RAOVATs.OrderByDescending(x => x.TIEUDE).Where(s => s.MATRANGTHAI == 1);
+                return View(list);
+            }
+            else
+            {
+                var list = db.RAOVATs.OrderByDescending(x => x.TIEUDE).Where(x => x.CATEGORY.TENLOAI == category && x.MATRANGTHAI == 1);
+                return View(list);
+            }
+        }
+        public ActionResult Search(string keyword)
+        {
+            var search = db.RAOVATs.Where(x => x.TIEUDE.Contains(keyword)).ToList();
+            return View(search);
+        }
+        public PartialViewResult CategoryPartial()
+        {
+            var catelist = db.CATEGORies.ToList();
+            return PartialView(catelist);
+        }
+    }
+}
